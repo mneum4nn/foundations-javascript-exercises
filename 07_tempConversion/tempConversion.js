33d3e147cc6e357c6468c894b9d5ca0a61fe65ae
mneum4nn <n3um4nn@proton.me>
2023-09-27 12:00:40 -0600

    Implement if/else to determine if rounding is necessary

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let cels=0;
+  if (typeof fahrenheit == "number"){
+    cels = (9/5)*(fahrenheit-32);
+    if (cels%1 != 0){
+      cels = (((9/5)*(fahrenheit-32))*10)/10;
+    }
+    return cels;
+  } else {
+  return Error;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahr=0;
+  if (typeof fahrenheit == "number"){
+    fahr = (9/5)*(celsius+32);
+    if (fahr%1 != 0){
+      fahr = (((9/5)*(celsius+32))*10)/10;
+    }
+    return fahr;
+  } else {
+    return Error;
+  }
 };
 
 // Do not edit below this line
