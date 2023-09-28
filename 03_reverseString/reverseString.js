c392c253de131a57806a8cecf86c05fb5236db44
mneum4nn <n3um4nn@proton.me>
2023-09-28 13:23:40 -0600

    Complete functions and correct syntax errors

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let letters = str.split(""); 
+    let reversed = "";
+    for (let i = str.length-1; i>=0;i--){
+        reversed+=letters[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+    let newArr = [];
+    let toBeRemoved = [];
+    for (let i=1; i<=(arguments.length-1); i++){
+        toBeRemoved.push(arguments[i])
+        }
+    for (let j = 0; j<arr.length; j++){
+        if (toBeRemoved.indexOf(arr[j]) == -1){
+            newArr.push(arr[j]);
+            }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
